ab32d8d0e1cd4703179dc8078a2454d34f41ce99
Tyler Viducic <tyler.viducic@gmail.com>
2019-08-21 11:14:42 -0400

    stated season stats

--- NHL_Scraper.py
+++ NHL_Scraper.py
@@ -534,12 +534,6 @@ class Game:
         for play in self.game_plays:
             play.parse_play(self.teams)
 
-    def __add_inactive_skater__(self, player_type, skater):
-        if player_type == 'Goalie':
-            self.__add_inactive__goalie__(skater)
-        else:
-            self.__add_inactive__player__(skater)
-
 
 class SeasonRoster:
 
@@ -573,3 +567,79 @@ class SeasonRoster:
         for person in team_dict['teams'][0]['roster']['roster']:
             players.append(person['person']['fullName'])
         return players
+
+class PlayerSeason:
+
+    def __init__(self, player_name, player_team, season_start_year):
+        self.player_name = player_name
+        self.player_team = player_team
+        self.season_start = season_start_year
+        self.games_list = []
+        self.__get_games_list__(self.season_start)
+
+    # Private methods
+
+    def __get_games_list__(self, season_start):
+        date = datetime.date(season_start, 10, 1)
+        while date < datetime.date(season_start + 1, 4, 5):
+            self.__add_players_games__(date)
+            date += 1
+
+    def __add_players_games__(self, date):
+        daily_games = DailySchedule(date.year, date.month, date.day)
+        if daily_games.did_team_play(self.player_team):
+            self.games_list.append(daily_games.get_game_by_teamname(self.player_team))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
